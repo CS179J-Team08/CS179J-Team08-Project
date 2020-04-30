b9739a7a3faeb1c5dcb315d0d8d1a1535086e0dd
jd <jdriv90@gmail.com>
2020-04-30 16:06:53 -0700

    fixed companion app issue

--- companion-app/src/Components/UploadFile.js
+++ companion-app/src/Components/UploadFile.js
@@ -3,15 +3,6 @@ import { Storage, API, graphqlOperation } from 'aws-amplify'
 import uuid from 'uuid/v4'
 import { withAuthenticator } from 'aws-amplify-react'
 
-import { createProduct as CreateProduct } from './graphql/mutations'
-import { listProducts as ListProducts } from './graphql/queries'
-import config from './aws-exports'
-
-const {
-  aws_user_files_s3_bucket_region: region,
-  aws_user_files_s3_bucket: bucket
-} = config;
-
 class UploadFile extends Component {
   /*
     uploadFile = (evt) => {
