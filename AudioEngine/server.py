896763c544c544c2ba5a445a1ae66707f7f9b4d7
arent128 <arent007@ucr.edu>
2020-05-04 02:51:08 -0700

    Finished the current goal for sockets, now moving to JSON integration
    The current goal for this commit was to get a working sample of socket connections that would allow for our c++ program running on the pi to request for a file to play from a python program using sockets. Currently the sample uses alot of debug messages to visualize the back and forth between the two process. The python program currently only downloads a single file and finishes execution, while the c++ program plays the a given file on repeat until the process is forced to terminate.
    
    Things that need to be done
    *Begin to work on integrating JSON downloads instead of just a file.
    *Test the JSON working version before integrating the SQS queues that Brandon and Joseph have been working on.
    *Implement more error checking in the python program in regards to buckets, files, and paths existing.
    *See whether or not the calls for sleep and time.sleep are needed for the program to function.

--- AudioEngine/server.py
+++ AudioEngine/server.py
@@ -1,14 +1,18 @@
 import boto3
 import sys
 import socket
+import time
 
+#Definitions of the HOST and PORT to use for the socket connection
 HOST = 'localhost'	# Symbolic name meaning all available interfaces
-PORT = 8888	# Arbitrary non-privileged port
+PORT = 12345	# Arbitrary non-privileged port
 
+
+#This method begins the socket creation. We are using a TCP connection given the SOCK_STREAM option
 def socket_server_init():
 # Datagram (udp) socket
     try :
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print ('Socket created')
         return s
     except socket.error as msg :
@@ -16,82 +20,133 @@ def socket_server_init():
         sys.exit()
 
 
-
+#This method initiates the creation of the python server.
 def socket_server_accept_connection(s):
-# Bind socket to local host and port
+    #The response message to be sent back. The response must be sent as of data type bytes.
+    #Here the bytes method formats the first arguement into the form of b'...' with the utf-8 encoding
     vaild_connection = bytes("Client connected to the server", 'utf-8')
     try:
+        #Use the given HOST and PORT to bind to file descriptor
         s.bind((HOST, PORT))
+        #Listen to the file descriptor for a client connection
+        s.listen(5)
+        print("waiting for client")
+        #Here be use the accept method to confirm the connection with a client
+        conn, addr = s.accept()
+        print ("Connected by" + str(addr))
     except socket.error as msg:
         print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
         sys.exit()
-	
+
+    #Here we specify that we want to receive data from our cliemt connection
+    #The recv call is a blocking call and will hang unless a response is given.
+    #Reading 1024 bytes worth of information from our client. It might not need to be this big
     print ('Socket bind complete')
+    d = conn.recv(1024)
+    #The information from our client is also in a byte format (b'...'). It must be decoded as shown below
+    data = d.decode()
 
-    d = s.recvfrom(1024)
-    data = d[0]
-    addr = d[1]
-    print (str(data))
+    #The following is the current manner in which data is sent back and forth. Some of these statements
+    #are not really necessary, however this is to illustrate the back and forth communication between processes
+    #We begin by first sending the c++ program the size our response, then we send the actual response.
+    #This was done to ensure that the c++ program can actually continue to send and receive data without the socket dying
+    #We implement some delays to visualize the back and forth, though I am not sure if they are needed to ensure a properly working system.
+    length = len(vaild_connection.decode())
+    print (d)
+    print("The current length of the vaild connection response " + str(length))
+    time.sleep(8)
+    s.close()
     if not data: 
         print("Error receiving client data")
-        s.close()
         sys.exit()
     else:
-        #reply = vaild_connection
-        s.sendto(vaild_connection, addr)
-        print ('Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + str(data).strip())
+        conn.sendall(bytes(str(length), 'utf-8'))
+        time.sleep(5)
+        conn.sendall(vaild_connection)
+        print ("Message sent to the client")
+    return conn
 
-def socket_server_await_request(s):
+#Similar to the accept connection method we wait until a response from the c++ program requesting a file for playback. We still use a similar method of receiving and sending responses as done in accept_connection
+def socket_server_await_request(conn):
+    response = bytes("Getting a file for playback", 'utf-8')
     print("Awaitng next request")
-    d = s.recvfrom(1024)
-    data = d[0]
-    addr = d[1]
+    d = conn.recv(1024)
+    data = d.decode()
+    length = len(response.decode())
+    print("The current length of response: " + str(length))
+    print(data)
+    time.sleep(5)
     if not data: 
         print("Error receiving client data")
-        s.close()
         sys.exit()
-    if str(data) is "Connection to server from AudioEngine":
-        reply = vaild_connection
-        s.sendto(reply, addr)
-        print ('Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + str(data).strip())
-        return addr
     else:
-        return -1
+        conn.sendall(bytes(str(length), 'utf-8'))
+        time.sleep(5)
+        conn.sendall(bytes("Getting a file for playback", 'utf-8'))
+        print("Request acknowledged beginning file download")
 
-def socket_server_respond_request(s, addr, fileName):
+
+#This method is the response sent back to the c++ that initially requested a file for playback. It takes in the current connection and the fileName to send.
+def socket_server_respond_request(conn, fileName):
     reply = bytes(fileName, 'utf-8')
-    s.sendto(reply, addr)
+    length = len(reply.decode())
+    print("Length of file to play: " + str(length))
+    print(str(reply))
+    time.sleep(5)
+    conn.sendall(bytes(str(length), 'utf-8'))
+    time.sleep(5)
+    conn.sendall(reply)
 
-
+#The list_AWS_buckets method uses a boto3 client to print a list of the current bucket that exist on the S3 cloud service.
 def list_AWS_buckets():
+    #Initialze the s3 client
     s3 = boto3.client('s3')
+    #The list_buckets method returns a dict of the buckets that exist in S3
     response = s3.list_buckets()
+    #Begin to iterate over the dict and buckets and print out the name of each bucket
     print('Existing Buckets:')
     for bucket in response['Buckets']:
         print(f'  {bucket["Name"]}')
 
+#The aws_download method takes in a bucketName to look at, a fileName that represent a file path to search for in the given bucket, and storageResult as the user specified file name for the resulting download to have.
 
+#Returns the storageResult string name
 def aws_download(bucketName, fileName, storageResult):
+    #Initialize a boto3 resource
     s3 = boto3.resource('s3')
     print(fileName)
+    #Begin to download the specified fileName
     s3.meta.client.download_file(bucketName, fileName, storageResult)
     print(storageResult,"was downloaded successfully!")
     return storageResult
 
+
+#Request_data returns a tuple of the fileName as a string, and the storageResult for what to name the downloaded file
 def request_data():
     fileName = input("Enter the file to download: ")
     storageResult = input("What do you want to name the downloaded file: ")
     return (fileName, storageResult)
 
+
+#confirm_file_is_vaild is method that takes in a bucket to target, the bucket as an object to iterate over, and a tuple called target_data that represents the fileName, and storageResult we want from a given bucket.
+
+#Returns a string called fileName that contains the user specified file name.
 def confirm_file_is_vaild(bucket_path, target_bucket, target_data):
+    #Iterate over the object looking for the particular fileName in target_data. (fileName = target_data[0])
     for obj in target_bucket.objects.all():
         if target_data[0] in obj.key:
+            #If the string of the fileName exists in the obj.key then we have found the file we want
+            #We call aws_download and pass the bucket we want to download from, the exact path to the file for download, and the user specified file name for the given download
             fileName = aws_download(bucket_path, obj.key, target_data[1])
             return fileName
     print ("file not found")
 
+
+#bucket_menu runs the input and output needed for file downloads
+#Returns a fileName of the file that was downloaded named by the user.
 def bucket_menu():
     s3 = boto3.resource('s3')
+    list_AWS_buckets()
     bucket_path = input("Which bucket do you want to look into?: ")
     target_bucket = s3.Bucket(bucket_path)
     for obj in target_bucket.objects.all():
@@ -101,22 +156,22 @@ def bucket_menu():
     return fileName
 
 
+#Main function
 if __name__ == '__main__':
     try:
+        #Initialize our socket and connection to None, then begin their initialization
         s = None
+        conn = None
         s = socket_server_init()
-        socket_server_accept_connection(s)
-        s3 = boto3.resource('s3')
-        while 1:
-       #         list_AWS_buckets()
-       #         fileName = bucket_menu()
-       #         addr = socket_server_await_request(s)
-       #         if addr is -1:
-       #             print("Error could not resolve address of client")
-       #         else:
-       #             socket_server_respond_request(s, addr, fileName)
-        s.close()
+        conn = socket_server_accept_connection(s)
+        #Wait for a client request asking for file playback
+        socket_server_await_request(conn)
+        #Once acknowledged, we can begin our file download
+        fileName = bucket_menu()
+        #Now that we have a file for playback, send the name of the file over the socket connection
+        socket_server_respond_request(conn, fileName)
         sys.exit()
 
+#This can shutdown gracefully if you press control + c.
     except KeyboardInterrupt:
         s.close()
