b3c0e1cb9ac7f8c7271835a6a01d6133f1cabbeb
jd <jdriv90@gmail.com>
2020-05-12 01:10:41 -0700

    added effects to data packet; web app now sends effect parameters

--- companion-app/src/Components/UploadFile.js
+++ companion-app/src/Components/UploadFile.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useState, Component } from 'react';
+import React, { Component } from 'react';
 import { Storage, API, graphqlOperation, Auth } from 'aws-amplify'
-import uuid from 'uuid/v4'
-import { withAuthenticator } from 'aws-amplify-react'
 import * as mutations from '../graphql/mutations';
 import * as queries from '../graphql/queries';
 
@@ -16,9 +14,22 @@ let packet = {
   ],
   filename: "",
   play: false,
-//  stop: false,
+  stop: false,
   parameters: {
-    volume: 0.0
+    volume: 0.0,
+    echo: {
+      apply: false,
+      delay: 10.0,
+      feedback: 0.0,
+      dry: 0.0,
+      wet: 0.0
+    },
+    equalizer: {
+      apply: false,
+      lowgain: 0.0,
+      midgain: 0.0,
+      highgain: 0.0
+    }
   }
 };
 
@@ -35,14 +46,32 @@ class UploadFile extends Component {
 
         this.state = {
           Filename: "",
-          playValue: 'false',
-          volume: 0.0
+          playValue: "true",
+          stop: "false",
+          volume: 0.0,
+          eqapply: "false",
+          eqlowgain: 0.0,
+          eqmidgain: 0.0,
+          eqhighgain: 0.0,
+          echoapply: "false",
+          echodelay: 10.0,
+          echofeedback: 0.0,
+          echodry: 0.0,
+          echowet: 0.0
         };
 
         this.updatePlayState = this.updatePlayState.bind(this);
         this.updateFileName = this.updateFileName.bind(this);
         this.updateVolume = this.updateVolume.bind(this);
-
+        this.applyEq = this.applyEq.bind(this);
+        this.updateHighEq = this.updateHighEq.bind(this);
+        this.updateMidEq = this.updateMidEq.bind(this);
+        this.updateLowEq = this.updateLowEq.bind(this);
+        this.applyEcho = this.applyEcho.bind(this);
+        this.updateEchoDelay = this.updateEchoDelay.bind(this);
+        this.updateEchoFeedback = this.updateEchoFeedback.bind(this);
+        this.udpateEchoWetVolume = this.udpateEchoWetVolume.bind(this);
+        this.udpateEchoDryVolume = this.udpateEchoDryVolume.bind(this);
       }
 
       uploadAudioFile = async (evt) => {
@@ -57,7 +86,7 @@ class UploadFile extends Component {
         // check if file name already exists for current user
         var fileExists = false;
         for (const i in items) {
-          if(items[i].name == name) {
+          if(items[i].name === name) {
             fileExists = true;
           }
         }
@@ -98,13 +127,21 @@ class UploadFile extends Component {
       sendPacket = () => {
         packet.filename = this.state.Filename;
         packet.play = this.state.playValue;
+        packet.stop = this.state.stop;
         packet.parameters.volume = this.state.volume;
+        packet.parameters.equalizer.apply = this.state.eqapply;
+        packet.parameters.equalizer.lowgain = this.state.eqlowgain;
+        packet.parameters.equalizer.midgain = this.state.eqmidgain;       
+        packet.parameters.equalizer.highgain = this.state.eqhighgain;
+        packet.parameters.echo.apply = this.state.echoapply;
+        packet.parameters.echo.delay = this.state.echodelay;
+        packet.parameters.echo.feedback = this.state.echofeedback;
+        packet.parameters.echo.wet = this.state.echowet;
+        packet.parameters.echo.dry = this.state.echodry;
         
         // debugging purposes
-        console.log(packet.group[0].userID);
-        console.log(packet.filename);
-        console.log(packet.play);
-        console.log(packet.volume);
+        console.log(packet.parameters.equalizer);
+        console.log(packet.parameters.echo);
 
         Storage.put(jsonFilePrefix + '.json', packet)
         .then (result => console.log(result))
@@ -112,7 +149,17 @@ class UploadFile extends Component {
       }
 
       updatePlayState(event) {
-        this.setState({ playValue: event.target.value });
+        if(event.target.value === "stop") {
+          this.setState({ stop: "true" });
+        }
+
+        if(event.target.value === "play") {
+          this.setState({ playValue: "true", stop: "false" });
+        }
+
+        if(event.target.value === "pause") {
+          this.setState({ playValue: "false", stop: "false" });
+        }
       }
 
       updateFileName(event) {
@@ -123,34 +170,106 @@ class UploadFile extends Component {
         this.setState({ volume: event.target.value });
       }
 
+      applyEq(event) {
+        this.setState({ eqapply: event.target.checked });
+      }
+
+      updateLowEq(event) {
+        this.setState({ eqlowgain: event.target.value });
+      }
+
+      updateMidEq(event) {
+        this.setState({ eqmidgain: event.target.value });
+      }
+
+      updateHighEq(event) {
+        this.setState({ eqhighgain: event.target.value });
+      }
+
+      applyEcho(event) {
+        this.setState({ echoapply: event.target.checked });
+      }
+
+      updateEchoDelay(event) {
+        this.setState({ echodelay: event.target.value });
+      }
+      
+      updateEchoFeedback(event) {
+        this.setState({ echofeedback: event.target.value });
+      }
+
+      udpateEchoWetVolume(event) {
+        this.setState({ echowet: event.target.value });
+      }
+
+      udpateEchoDryVolume(event) {
+        this.setState({ echodry: event.target.value });
+      }
+
       render() {
         return (
             <>
               <div>
-                <h3>Upload a file</h3>
+                <h2>Upload a file</h2>
                 <input type="file" onChange={this.uploadAudioFile} />
               </div>
               <div>
-                <h3>Send file to device</h3>
-                <form>
-                  <label>
-                    Filename:
-                    <input type="text" onChange={this.updateFileName} />
-                  </label>
-                  <label>
-                    Play/Stop
-                    <select value={this.state.playValue} onChange={this.updatePlayState}>
-                      <option value="true">play</option>
-                      <option value="false">stop</option>
-                    </select>
-                  </label>
-                  <label>
-                    Volume:
-                    <input type="number" step="0.1" onChange={this.updateVolume} />
-                  </label>
-                </form>
-                <button onClick={this.sendPacket}>Send</button>
+                <h2>Play file</h2>
+                <label>
+                  Filename:
+                  <input type="text" onChange={this.updateFileName} />
+                </label>
+                <label>
+                  Play/Pause/Stop
+                  <select onChange={this.updatePlayState}>
+                    <option value="play">play</option>
+                    <option value="pause">pause</option>
+                    <option value="stop">stop</option>
+                  </select>
+                </label>
+                <label>
+                  Volume:
+                  <input type="number" step="0.1" onChange={this.updateVolume} />
+                </label>
               </div>
+              <h3>Effects</h3>
+              <div>
+                <h5>EQ:</h5>
+                <input type="checkbox" onChange={this.applyEq} />
+                <label>
+                  Low Gain:
+                  <input type="number" step="0.1" onChange={this.updateLowEq} />
+                </label>
+                <label>
+                  Mid Gain:
+                  <input type="number" step="0.1" onChange={this.updateMidEq} />
+                </label>
+                <label>
+                  High Gain:
+                  <input type="number" step="0.1" onChange={this.updateHighEq} />
+                </label>
+              </div>
+              <div>
+                <h5>Echo:</h5>
+                <input type="checkbox" onChange={this.applyEcho} />
+                <label>
+                  Delay:
+                  <input type="number" step="0.1" onChange={this.updateEchoDelay} />
+                </label>
+                <label>
+                  Feedback:
+                  <input type="number" step="0.1" onChange={this.updateEchoFeedback} />
+                </label>
+                <label>
+                  Wet Volume:
+                  <input type="number" step="0.1" onChange={this.udpateEchoWetVolume} />
+                </label>
+                <label>
+                  Dry Volume:
+                  <input type="number" step="0.1" onChange={this.udpateEchoDryVolume} />
+                </label>
+              </div>
+              <button onClick={this.sendPacket}>Update Audio</button>
             </>
         );
       }
