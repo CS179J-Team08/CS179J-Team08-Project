d181502a9554a25c077aba509d883a8e2ad3d3c2
jd <jdriv90@gmail.com>
2020-05-18 23:49:56 -0700

    added routers, users can now choose between solo and group

--- companion-app/src/App.js
+++ companion-app/src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-//import { Button } from 'react-bootstrap';
 import './App.css';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
-import config from './aws-exports';
-import { withAuthenticator, S3Image } from 'aws-amplify-react';
-import { API, graphqlOperation, Storage } from 'aws-amplify';
-import { Form, Grid, Header, Input, List, Segment } from 'semantic-ui-react';
-import { v4 as uuid } from 'uuid';
+import { withAuthenticator } from 'aws-amplify-react';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import UploadFile from "./Components/UploadFile";
 import ListFiles from "./Components/ListFiles";
+import GroupPlay from "./Components/GroupPlay"
 
 Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
     return (
-     <div className="App">
-       <UploadFile />
-       <ListFiles />
-     </div>
+      <>
+        <Router>
+          <Link to="/"> Home </Link>
+          <Link to="/solo-play"> Solo Play </Link>  
+          <Link to="/group-play"> Group Play </Link>  
+          <Route exact path="/solo-play" render={ props =>
+            <div>
+              <UploadFile />
+              <ListFiles /> 
+            </div> 
+          } />
+          <Route exact path="/group-play" render={ props =>
+              <div>
+                <GroupPlay />
+              </div>
+            } />
+        </Router>
+      </>
     );
   }
 }
 
 export default withAuthenticator(App, true);
+ 
---
+++ companion-app/src/Components/GroupPlay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Auth } from 'aws-amplify';
+
+export default class App extends Component {
+    render() {
+      return (
+        <>
+            <div>
+                <p>TODO: Add group play functionality</p>
+            </div>
+        </>
+      );
+    }
+  }
